d69d58dc48b754a236625893aef3039c3af65e85
ANUJ CHAURASIA <79889686+rac1004@users.noreply.github.com>
2026-02-01 12:05:00 +0530

    Update hello_world.js

--- chatBot/hello_world.js
+++ chatBot/hello_world.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { OpenAI} from 'openai';
 
 const client =new OpenAI({
-    //faskfkaapiKey: "your gemini api key",
+    apiKey: "your gemini api key",
     baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
 });
 
